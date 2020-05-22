c3d95e71353ae89d838a14a3b63689f06b474656
Brandon Collins <brandoncollins@Brandons-iMac.local>
2020-05-21 21:32:25 -0400

    Updates

---
+++ precache-manifest.8368e5bea3b6371d742660e620eb4cc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458bc96864ee9c3bb5177cf352ceb795",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbf2b6ba8c011a1ddc25",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d89edd4f815e94ef344c",
+    "url": "/static/js/2.9ed52bd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9ed52bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf2b6ba8c011a1ddc25",
+    "url": "/static/js/main.cf6d6c7a.chunk.js"
+  },
+  {
+    "revision": "7129a994307f4aa325e4",
+    "url": "/static/js/runtime-main.c8f32266.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
